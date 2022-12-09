09a8c77883d79d24c2cbf215f5edcc863a45a269
Oliver Greenwood <oliver.greenwood@challoners.org>
2022-12-09 20:25:04 +0000

    first commit

---
+++ DebugScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugScreen : MonoBehaviour
+{
+
+    World world;
+    Text text;
+
+    int frameRate;
+    float timer;
+
+    int halfWorldSizeInVoxels;
+    int halfWorldSizeInChunks;
+
+    void Start() {
+
+        world = GameObject.Find("World").GetComponent<World>();
+        text = GetComponent<Text>();
+
+        halfWorldSizeInVoxels = VoxelData.WorldSizeInVoxels / 2;
+        halfWorldSizeInChunks = VoxelData.WorldSizeInChunks / 2;
+        
+    }
+
+    void Update() {
+
+        string debugText = "My Debug Screen:";
+        debugText += "\n";
+        debugText += frameRate + " fps";
+        debugText += "\n";
+        debugText += "XYZ: " +  Mathf.FloorToInt(world.player.transform.position.x - halfWorldSizeInVoxels) + " " +
+                                Mathf.FloorToInt(world.player.transform.position.y) + " " +
+                                Mathf.FloorToInt(world.player.transform.position.z - halfWorldSizeInVoxels);
+        debugText += "\n";
+        debugText += "Chunk: [" + (world.playerChunkCoord.x - halfWorldSizeInChunks) + ", " + (world.playerChunkCoord.z - halfWorldSizeInChunks) + "]";
+
+        text.text = debugText;
+
+        if (timer > 1f) {
+
+            frameRate = (int)(1f / Time.unscaledDeltaTime);
+            timer = 0;
+
+        } else {
+
+            timer += Time.deltaTime;
+
+        }
+        
+    }
+}
